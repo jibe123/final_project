829947602f87e85fba401b7a5d0bf8851941c7b8
jibe123 <jibek.k@gmail.com>
2022-12-07 08:26:59 +0600

    updated assignments

--- assignments/urls.py
+++ assignments/urls.py
@@ -1,6 +1,4 @@
-from django.urls import path, include, re_path
-from rest_framework.routers import DefaultRouter
-
+from django.urls import path, re_path
 import assignments.views as vs
 
 urlpatterns = [
@@ -10,15 +8,3 @@ urlpatterns = [
     re_path(r'quizzes/(?P<slug>[\w\-]+)/save-answer/$', vs.SaveAnswer.as_view()),
     re_path(r'quizzes/(?P<slug>[\w\-]+)/submit/$', vs.SubmitAssignmentAPI.as_view()),
 ]
-
-# router = DefaultRouter()
-#
-# router.register('assignments', vs.AssignmentViewSet, basename='assignments')
-# router.register('questions', vs.QQViewSet, basename='qq')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# path('assignments_graded/', vs.GradedAssignmentListView.as_view()),
-# path('assignments_grade/', vs.GradedAssignmentCreateView.as_view()),
-
-# ]
