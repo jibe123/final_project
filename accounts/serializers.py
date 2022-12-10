ac435626072300a832198491fb840874b2f72a98
jibe123 <jibek.k@gmail.com>
2022-12-10 23:52:16 +0600

    added average grade statistics to accounts app

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,27 +6,54 @@ from .models import User, Student, Group
 
 
 class StudentSerializer(sz.ModelSerializer):
-    user = sz.PrimaryKeyRelatedField(
-        queryset=User.objects.filter(is_student=True))
+    user = sz.StringRelatedField(read_only=True)
+    average = sz.SerializerMethodField()
 
     class Meta:
         model = Student
-        fields = ('user', 'group',)
+        fields = ('user', 'user_id', 'group', 'average')
+
+    def get_average(self, obj):
+        graded_assignments = obj.st_assignments.filter(graded=True)
+        count = 0
+        sum = 0
+        if graded_assignments:
+            for item in graded_assignments:
+                sum += item.grade
+                count += 1
+            return sum / count
+        return "У этого студента нет выполненных заданий"
 
 
 class GroupSerializer(sz.ModelSerializer):
     students = StudentSerializer
+    average = sz.SerializerMethodField()
 
     class Meta:
         model = Group
-        fields = 'id', 'name', 'students'
+        fields = 'id', 'name', 'students', 'average'
 
     def validate(self, data):
         for student in data['students']:
-            if student.group is not None:
+            if student.group is not None and \
+                    data['name'] != student.group.name:
                 raise sz.ValidationError(
                     'Данный студент уже состоит в другой группе!')
         return data
+
+    def get_average(self, obj):
+        count = 0
+        sum = 0
+        for item in obj.students.all():
+            graded_assignments = item.st_assignments.filter(graded=True)
+
+            if graded_assignments:
+                for item2 in graded_assignments:
+                    sum += item2.grade
+                    count += 1
+                    return sum / count
+
+        return "У группы пока нет выполненных заданий"
 
 
 class NewStudentUserSerializer(sz.ModelSerializer):
